7a9b1eedcefff11d6fed6b60e1ece4f5e68e654d
Corey Wolff <rottenApple@Coreys-Air.lan>
2018-08-02 12:47:46 +0800

    Added painting code

---
+++ Assets/Caster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caster : MonoBehaviour {
+	
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			Cast (); 
+		}
+			
+	}
+
+	void Cast(){
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition); 
+		RaycastHit hit; 
+		if (Physics.Raycast (ray, out hit)) {
+			Map map = hit.collider.gameObject.GetComponent<Map> (); 
+			if(map != null){
+				map.clickedOnPoint (hit.textureCoord); 
+			}
+		}
+	}
+}
---
+++ Assets/PTerrain.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PTerrain : MonoBehaviour {
+
+	static PTerrain t; 
+	[SerializeField]
+	Terrain terrain; 
+	[SerializeField]
+	int width; 
+	[SerializeField] 
+	int height; 
+	TerrainData terrainData; 
+	float[,] heightMap; 
+
+	void Awake(){
+		t = this; 
+	}
+
+	// Use this for initialization
+	void Start () {
+		heightMap = new float[width, height]; 
+		terrainData = terrain.terrainData; 
+		GenerateTerrain (); 
+	}
+
+	void GenerateTerrain(){
+		terrainData.SetHeights (width, height, heightMap); 
+	}
+
+	void ModifyHeightInternal(Vector2 point, int brushThickness){
+		int xStart = (int)(point.x * width);
+		int yStart = (int)(point.y * height);
+		int xEnd = Mathf.Min (width, brushThickness + xStart); 
+		int yEnd = Mathf.Min (height, brushThickness + yStart);
+		int avg = (xEnd - xStart + yEnd - yStart) / 2;
+		for (int x = xStart; x < xEnd; x++) {
+			for (int y = yStart; y < yEnd; y++) {
+				float height =  (float)(Mathf.Abs (x - xStart + y - yStart  - avg)) / (brushThickness * 2); 
+				heightMap [x, y] = height * 0.5f; 
+			}
+		}
+		terrainData.SetHeights (width, height, heightMap); 
+	}
+	public static void ModifyHeight(Vector2 point, int brushThickness){
+		t.ModifyHeightInternal(point, brushThickness); 
+	}
+
+}
